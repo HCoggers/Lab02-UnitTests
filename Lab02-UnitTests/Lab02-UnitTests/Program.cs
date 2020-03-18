02a8966b2e0040bd50bf24822c5004afd119d11f
Harry Cogswell <harry.cogswell@gmail.com>
2020-03-17 18:40:58 -0700

    builds basic methods

--- Lab02-UnitTests/Lab02-UnitTests/Program.cs
+++ Lab02-UnitTests/Lab02-UnitTests/Program.cs
@@ -8,5 +8,30 @@ namespace Lab02_UnitTests
         {
             Console.WriteLine("Hello World!");
         }
+        static void ATM()
+        {
+            decimal balance = 5000m;
+            Console.WriteLine("Welcome to my Magical ATM App" +
+                "\nPlease choose a transaction by selecting a number:");
+            string transaction = Console.ReadLine();
+            switch(transaction)
+            {
+
+            }
+        }
+        static decimal GetBalance(decimal balance)
+        {
+            return balance;
+        }
+        static decimal Withdraw(decimal moneyTaken, decimal balance)
+        {
+            decimal newBalance = balance - moneyTaken;
+            return newBalance;
+        }
+        static decimal Deposit(decimal moneyGiven, decimal balance)
+        {
+            decimal newBalance = balance + moneyGiven;
+            return newBalance;
+        }
     }
 }
