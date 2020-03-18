b6b98bf8809ee877612fef5b193e26be71aba67a
Harry Cogswell <harry.cogswell@gmail.com>
2020-03-17 19:41:33 -0700

    Catches overflow and format exceptions, and adds custom exceptions for balance dropping below 0 and receiving bad user input

--- Lab02-UnitTests/Lab02-UnitTests/Program.cs
+++ Lab02-UnitTests/Lab02-UnitTests/Program.cs
@@ -13,8 +13,8 @@ namespace Lab02_UnitTests
             }
             catch(Exception e)
             {
-                Console.WriteLine("Something went wrong." +
-                    $"\nThe exception was: {e.Message}");
+                Console.WriteLine("Something went wrong: " +
+                    $"\n{e.Message}");
             }
             finally
             {
@@ -23,52 +23,70 @@ namespace Lab02_UnitTests
         }
         static void ATM(decimal balance = 5000)
         {
-            bool reload = true;
-            Console.Clear();
-            Console.WriteLine("Welcome to my Magical ATM App" +
-                "\nPlease choose a transaction by selecting a number:" +
-                "\n" +
-                "\n1) View your current balance" +
-                "\n2) Make a Withdrawal" +
-                "\n3) Make a Deposit" +
-                "\n\t...or press \"X\" to exit");
-            string transaction = Console.ReadLine();
-            switch(transaction)
+            try
             {
-                case "1":
-                    balance = GetBalance(balance);
-                    Console.WriteLine($"Your balance is currently {balance}");
-                    break;
-                case "2":
-                    Console.WriteLine("How much cash would you like to Withdraw?");
-                    string cashTaken = Console.ReadLine();
-                    balance = Withdraw(cashTaken, balance);
-                    Console.WriteLine($"You Successfully Withdrew {cashTaken}, your balance is now {balance}");
-                    break;
-                case "3":
-                    Console.WriteLine("How much cash would you like to Deposit?");
-                    string cashGiven = Console.ReadLine();
-                    balance = Deposit(cashGiven, balance);
-                    Console.WriteLine($"You Successfully Deposited {cashGiven}, Your balance is currently {balance}");
-                    break;
-                case "x":
-                    reload = false;
-                    break;
-            }
-            // Keep asking the user to pick a transaction until they choose to exit the application
-            if (reload)
-            {
-                string choice;
-                do
+                bool reload = true;
+                Console.Clear();
+                Console.WriteLine("Welcome to my Magical ATM App" +
+                    "\nPlease choose a transaction by selecting a number:" +
+                    "\n" +
+                    "\n1) View your current balance" +
+                    "\n2) Make a Withdrawal" +
+                    "\n3) Make a Deposit" +
+                    "\n\t...or press \"X\" to exit");
+                string transaction = Console.ReadLine();
+                switch (transaction)
                 {
-                    Console.WriteLine("Would you like to make another transaction? " +
-                       "\n\t...enter \"Y\" or \"N\"");
-                    choice = Console.ReadLine();
-                    if(choice == "y")
-                        ATM(balance);
+                    case "1":
+                        balance = GetBalance(balance);
+                        Console.WriteLine($"Your balance is currently {balance}");
+                        break;
+                    case "2":
+                        Console.WriteLine("How much cash would you like to Withdraw?");
+                        string cashTaken = Console.ReadLine();
+                        balance = Withdraw(cashTaken, balance);
+                        Console.WriteLine($"You Successfully Withdrew {cashTaken}, your balance is now {balance}");
+                        break;
+                    case "3":
+                        Console.WriteLine("How much cash would you like to Deposit?");
+                        string cashGiven = Console.ReadLine();
+                        balance = Deposit(cashGiven, balance);
+                        Console.WriteLine($"You Successfully Deposited {cashGiven}, Your balance is currently {balance}");
+                        break;
+                    case "x":
+                        reload = false;
+                        break;
+                    default:
+                        throw (new Exception("That wasn't an option." +
+                            "\nTransaction Cancelled."));
+                }
+                // Keep asking the user to pick a transaction until they choose to exit the application
+                if (reload)
+                {
+                    string choice;
+                    do
+                    {
+                        Console.WriteLine("Would you like to make another transaction? " +
+                           "\n\t...enter \"Y\" or \"N\"");
+                        choice = Console.ReadLine();
+                        if (choice == "y")
+                            ATM(balance);
 
-                } while (choice != "y" && choice != "n");
-                
+                    } while (choice != "y" && choice != "n");
+
+                }
+            }
+            catch(FormatException e)
+            {
+                Console.WriteLine("Please enter a dollar amount." +
+                    "\nTransaction Cancelled due to:" +
+                    $"\n{e.Message}");
+            }
+            catch(OverflowException e)
+            {
+                Console.WriteLine("I can't legally process that much." +
+                    "\nTransaction Cancelled due to:" +
+                    $"\n{e.Message}");
             }
         }
         static decimal GetBalance(decimal balance)
@@ -78,7 +96,12 @@ namespace Lab02_UnitTests
         static decimal Withdraw(string cashTaken, decimal balance)
         {
             decimal money = Convert.ToDecimal(cashTaken);
-            decimal newBalance = balance - money;
+            decimal newBalance;
+            if ((balance - money) >= 0)
+                newBalance = balance - money;
+            else
+                throw (new Exception("You don't have that much money." +
+                    "\nTransaction Cancelled."));
             return newBalance;
         }
         static decimal Deposit(string cashGiven, decimal balance)
