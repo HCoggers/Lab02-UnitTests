e5ec6d101e217337511d281320e52bca2a1e88a1
Harry Cogswell <harry.cogswell@gmail.com>
2020-03-17 19:16:40 -0700

    Fleshes out actions in methods. adds catch block for all generic exceptions.

--- Lab02-UnitTests/Lab02-UnitTests/Program.cs
+++ Lab02-UnitTests/Lab02-UnitTests/Program.cs
@@ -6,31 +6,85 @@ namespace Lab02_UnitTests
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            try
+            {
+                Console.WriteLine("Hello World!");
+                ATM();
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Something went wrong." +
+                    $"\nThe exception was: {e.Message}");
+            }
+            finally
+            {
+                Console.WriteLine("Have a nice day!");
+            }
         }
-        static void ATM()
+        static void ATM(decimal balance = 5000)
         {
-            decimal balance = 5000m;
+            bool reload = true;
+            Console.Clear();
             Console.WriteLine("Welcome to my Magical ATM App" +
-                "\nPlease choose a transaction by selecting a number:");
+                "\nPlease choose a transaction by selecting a number:" +
+                "\n" +
+                "\n1) View your current balance" +
+                "\n2) Make a Withdrawal" +
+                "\n3) Make a Deposit" +
+                "\n\t...or press \"X\" to exit");
             string transaction = Console.ReadLine();
             switch(transaction)
             {
+                case "1":
+                    balance = GetBalance(balance);
+                    Console.WriteLine($"Your balance is currently {balance}");
+                    break;
+                case "2":
+                    Console.WriteLine("How much cash would you like to Withdraw?");
+                    string cashTaken = Console.ReadLine();
+                    balance = Withdraw(cashTaken, balance);
+                    Console.WriteLine($"You Successfully Withdrew {cashTaken}, your balance is now {balance}");
+                    break;
+                case "3":
+                    Console.WriteLine("How much cash would you like to Deposit?");
+                    string cashGiven = Console.ReadLine();
+                    balance = Deposit(cashGiven, balance);
+                    Console.WriteLine($"You Successfully Deposited {cashGiven}, Your balance is currently {balance}");
+                    break;
+                case "x":
+                    reload = false;
+                    break;
+            }
+            // Keep asking the user to pick a transaction until they choose to exit the application
+            if (reload)
+            {
+                string choice;
+                do
+                {
+                    Console.WriteLine("Would you like to make another transaction? " +
+                       "\n\t...enter \"Y\" or \"N\"");
+                    choice = Console.ReadLine();
+                    if(choice == "y")
+                        ATM(balance);
 
+                } while (choice != "y" && choice != "n");
+                
             }
         }
         static decimal GetBalance(decimal balance)
         {
             return balance;
         }
-        static decimal Withdraw(decimal moneyTaken, decimal balance)
+        static decimal Withdraw(string cashTaken, decimal balance)
         {
-            decimal newBalance = balance - moneyTaken;
+            decimal money = Convert.ToDecimal(cashTaken);
+            decimal newBalance = balance - money;
             return newBalance;
         }
-        static decimal Deposit(decimal moneyGiven, decimal balance)
+        static decimal Deposit(string cashGiven, decimal balance)
         {
-            decimal newBalance = balance + moneyGiven;
+            decimal money = Convert.ToDecimal(cashGiven);
+            decimal newBalance = balance + money;
             return newBalance;
         }
     }
